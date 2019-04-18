a533ee6ce7fda4d47ac1726c8a05958a943aae73
reyunn <reyunn.yandex.com>
2019-04-18 12:53:57 +0800

    1.0.0

--- index.js
+++ index.js
@@ -1,34 +1,68 @@
 const express = require("express");
+const fs = require("fs");
+const bodyParser = require('body-parser')
 const app = express();
-const about = require("./conf/about.js");
-const a = require("./conf/a.js");
-const b = require("./conf/b.js");
-const c = require("./conf/c.js");
-const a_s = require("./conf/a_s.js");
-const b_s = require("./conf/b_s.js");
-const c_s = require("./conf/c_s.js");
+
+let a = JSON.parse(fs.readFileSync('./conf/a.json'));
+let b = JSON.parse(fs.readFileSync('./conf/b.json'));
+let c = JSON.parse(fs.readFileSync('./conf/c.json'));
+let about = JSON.parse(fs.readFileSync('./conf/about.json'));
+let a_s =JSON.parse(fs.readFileSync('./conf/a_s.json'));
+let b_s = JSON.parse(fs.readFileSync('./conf/b_s.json'));
+let c_s = JSON.parse(fs.readFileSync('./conf/c_s.json'));
+
 app.use(express.static('public'));
-app.engine("html",require("express-art-template"));
+app.engine("html", require("express-art-template"));
+const jsonParser = bodyParser.json()
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-app.get("/"     , (req,res)=>{ res.render("index.html");});
-app.get("/a_s.html",(req,res)=>{ res.render("a_s.html",{a_s:a_s});});
-app.get("/b_s.html",(req,res)=>{ res.render("b_s.html",{b_s:b_s});});
-app.get("/c_s.html",(req,res)=>{ res.render("c_s.html",{c_s:c_s});});
-app.get("/about.html",(req,res)=>{ res.render("about.html",{
-    url:about.url,
-    introduce:about.introduce,
-    vip_total1:about.vip_total1,
-    vip_total2:about.vip_total2,
-    vip_total3:about.vip_total3,
-    vip_total4:about.vip_total4,
-    timeline:about.timeline,
-    num_total:about.num_total
-});});
-app.get("/a.html",(req,res)=>{ res.render("a.html",{a:a});});
-app.get("/b.html",(req,res)=>{ res.render("b.html",{b:b});});
-app.get("/c.html",(req,res)=>{ res.render("c.html",{c:c});});
+app.get("/", (req, res) => {
+    res.render("index.html");
+});
+app.get("/a_s.html", (req, res) => {
+    res.render("a_s.html", {a_s: a_s});
+});
+app.get("/b_s.html", (req, res) => {
+    res.render("b_s.html", {b_s: b_s});
+});
+app.get("/c_s.html", (req, res) => {
+    res.render("c_s.html", {c_s: c_s});
+});
+app.get("/about.html", (req, res) => {
+    res.render("about.html", {
+        url: about.url,
+        introduce: about.introduce,
+        vip_total1: about.vip_total1,
+        vip_total2: about.vip_total2,
+        vip_total3: about.vip_total3,
+        vip_total4: about.vip_total4,
+        timeline: about.timeline,
+        num_total: about.num_total
+    });
+});
+app.get("/a.html", (req, res) => {
+    res.render("a.html", {a: a});
+});
+app.get("/b.html", (req, res) => {
+    res.render("b.html", {b: b});
+});
+app.get("/c.html", (req, res) => {
+    res.render("c.html", {c: c});
+});
+app.get("/admin", (req, res) => {
+    res.render("admin.html")
+});
+app.get("/admin_1", (req, res) => {
+    res.render("admin/1.html")
+});
+app.get("/get_about", (req, res) => {
+    res.send(about)
+});
 
-app.get("/admin",(req,res)=>{ res.render("admin.html")});
-app.listen("80" , ()=>{
-console.log("service start on port 80... ")
+app.post("/update1",jsonParser,(req,res)=>{
+    fs.writeFileSync("./conf/about.json",JSON.stringify(req.body));
+    res.send({"msg":"ok"});
+});
+app.listen("80", () => {
+    console.log("service start on port 80... ")
 });
